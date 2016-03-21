dbd7ebe6e0c87a55805f062477d738fd6bf457a8
sameera91 <sameera.bachoti@gmail.com>
2016-03-21 23:50:11 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,48 @@
+class FiguresController < ApplicationController
+
+    get '/figures/new' do
+      erb :"figures/new"
+    end
+
+    post '/figures' do
+      @figure = Figure.create(params[:figure])
+      if !params[:title][:name].empty?
+        @figure.titles << Title.create(params[:title])
+      end
+
+      if !params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(params[:landmark])
+      end
+
+      @figure.save
+    end
+
+    get '/figures' do
+      @figures = Figure.all
+      erb :"figures/index"
+    end
+
+    get '/figures/:id' do
+      @figure = Figure.find_by_id(params[:id])
+      erb :"figures/show"
+    end
+
+    get '/figures/:id/edit' do
+      @figure = Figure.find_by_id(params[:id])
+      erb :"figures/edit"
+    end
+
+    patch '/figures/:id' do
+      @figure = Figure.find_by_id(params[:id])
+      @figure.update(params[:figure])
+      if !params[:title][:name].empty?
+        @figure.titles << Title.new(params[:title])
+      end
+
+      if !params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.new(params[:landmark])
+      end
+      @figure.save
+      erb :"figures/show"
+    end
+end
---
+++ db/migrate/20160318184639_title_figures.rb
@@ -0,0 +1,8 @@
+class TitleFigures < ActiveRecord::Migration
+  def change
+    create_table :title_figures do |t|
+      t.integer :title_id
+      t.integer :figure_id
+    end
+  end
+end
